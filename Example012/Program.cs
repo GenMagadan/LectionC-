3f530283a5ec31262c0c2130135854ae59656bbf
Gennady <genchik111@yandex.ru>
2023-05-13 11:35:41 +0300

    Add Example012

---
+++ Example012/Program.cs
@@ -0,0 +1,50 @@
+﻿// Вид1
+void MethodOne()
+{
+   Console.WriteLine("Автор...");
+}
+// MethodOne();
+
+// Вид2
+void MethodTwo(string msg)
+{
+   Console.WriteLine(msg);
+}
+// MethodTwo("Текст сообщения...");
+
+void MethodTwoOne(string msg, int count)
+{
+   int i = 0;
+   while (i < count)
+   {
+      Console.WriteLine(msg);
+      i++;
+   }
+}
+// MethodTwoOne("Текст сообщения...", 4);
+// MethodTwoOne(count: 4, msg: "Текст сообщения...");
+
+// Вид3
+
+int MethodThree()
+{
+   return DateTime.Now.Year;
+}
+// int year = MethodThree();
+// Console.WriteLine(year);
+
+// Вид4
+
+string MethodFour(int count, string text)
+{
+   int i = 0;
+   string result = String.Empty;
+   while (i < count)
+   {
+      result = result + text;
+      i++;
+   }
+   return result;
+}
+string res = MethodFour(12, "text ");
+Console.WriteLine(res);
